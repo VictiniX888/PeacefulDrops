eeb0424bc9359ef46b24fee8134b71763ef2055e
VictiniX888 <victiniy@gmail.com>
2017-02-03 19:55:23 +0800

    Raw meat turns into Rotten Flesh after 5 minutes in the overworld

--- src/main/java/victinix/peacefuldrops/events/EventRottenFlesh.java
+++ src/main/java/victinix/peacefuldrops/events/EventRottenFlesh.java
@@ -2,19 +2,26 @@ package victinix.peacefuldrops.events;
 
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.item.ItemExpireEvent;
-import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class EventRottenFlesh {
 
+    private List<Item> meat = Arrays.asList(Items.BEEF, Items.PORKCHOP, Items.CHICKEN, Items.MUTTON, Items.RABBIT);
+
     @SubscribeEvent
-    public static void eventRottenFlesh(ItemTossEvent event) {
+    public void eventRottenFlesh(ItemExpireEvent event) {
 
         EntityItem item = event.getEntityItem();
-        if(item.getEntityItem().isItemEqual(new ItemStack(Items.PORKCHOP))) {
-            item.setEntityItemStack(new ItemStack(Items.ROTTEN_FLESH));
+        int stack = item.getEntityItem().stackSize;
+        if(meat.contains(item.getEntityItem().getItem())) {
+            event.setCanceled(true);
+            item.setEntityItemStack(new ItemStack(Items.ROTTEN_FLESH, stack));
         }
     }
 }
